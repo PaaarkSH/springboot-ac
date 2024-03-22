37ccd2d19ed828992a439b8324ce2737a07bfae5
psh950 <psh950@naver.com>
2024-03-22 23:32:03 +0900

    주문 할인과 도메인 개발

---
+++ src/main/java/com/example/springbootac/order/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.springbootac.order;
+
+
+import com.example.springbootac.discount.DiscountPolicy;
+import com.example.springbootac.discount.FixDiscountPolicy;
+import com.example.springbootac.member.Member;
+import com.example.springbootac.member.MemberRepository;
+import com.example.springbootac.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
