56a561f40c73e5e697bc473f7ba85217d0956e08
unknown <psh950@naver.com>
2024-03-26 00:13:43 +0900

    문제점과 해결방안

--- src/main/java/com/example/springbootac/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbootac/order/OrderServiceImpl.java
@@ -2,14 +2,18 @@ package com.example.springbootac.order;
 
 
 import com.example.springbootac.discount.DiscountPolicy;
-import com.example.springbootac.discount.FixDiscountPolicy;
+// import com.example.springbootac.discount.FixDiscountPolicy;
+// import com.example.springbootac.discount.RateDiscountPolicy;
 import com.example.springbootac.member.Member;
 import com.example.springbootac.member.MemberRepository;
 import com.example.springbootac.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
