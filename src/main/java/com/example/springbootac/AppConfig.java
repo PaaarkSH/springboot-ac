d7f7afb36ca64a7e11170ed1fecbc1346de07e93
psh950 <psh950@naver.com>
2024-04-13 22:35:12 +0900

    appconfig 적용

--- src/main/java/com/example/springbootac/AppConfig.java
+++ src/main/java/com/example/springbootac/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.springbootac;
 
+import com.example.springbootac.discount.DiscountPolicy;
 import com.example.springbootac.discount.FixDiscountPolicy;
 import com.example.springbootac.member.MemberService;
 import com.example.springbootac.member.MemberServiceImpl;
@@ -9,10 +10,18 @@ import com.example.springbootac.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
