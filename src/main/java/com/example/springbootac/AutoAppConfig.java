49110b5cbb456789e76b914dd35d2c4eb17a62d1
psh950 <psh950@naver.com>
2024-06-18 23:27:29 +0900

    ㅎㅏ던거

--- src/main/java/com/example/springbootac/AutoAppConfig.java
+++ src/main/java/com/example/springbootac/AutoAppConfig.java
@@ -8,14 +8,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        // 기존의 AppConfig 의 Configuration 자동 구성을 막기 위해 filter 사용
-        // 보통 이렇게 사용하지 않음
-        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = {Configuration.class})
-        // , basePackages = "com.example.springbootac"
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig  {
-//    @Bean(name = "memoryMemberRepository")
-//    MemoryMemberRepository memoryMemberRepository() {
-//        return new MemoryMemberRepository();
-//    }
+public class  AutoAppConfig  {
 }
--- src/main/java/com/example/springbootac/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbootac/order/OrderServiceImpl.java
@@ -6,35 +6,22 @@ import com.example.springbootac.discount.DiscountPolicy;
 import com.example.springbootac.member.Member;
 import com.example.springbootac.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(
-            MemberRepository memberRepository,
-            @MainDiscountPolicy DiscountPolicy discountPolicy
-    ) {
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
-
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
-
-     @Override
+    @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
