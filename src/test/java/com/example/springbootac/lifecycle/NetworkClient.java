0d699b876759d62365cde8b2973567d42252d779
psh950 <psh950@naver.com>
2024-06-19 23:43:53 +0900

    일단 하던거

---
+++ src/test/java/com/example/springbootac/lifecycle/NetworkClient.java
@@ -0,0 +1,32 @@
+package com.example.springbootac.lifecycle;
+
+import java.io.IOException;
+
+public class NetworkClient {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("url: " + this.url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 네트워크 시작시 호출
+    public void connect() {
+        System.out.println("connect url: " + this.url) ;
+    }
+
+    public void call(String message) {
+        System.out.println("connect url: " + this.url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + this.url) ;
+    }
+}
