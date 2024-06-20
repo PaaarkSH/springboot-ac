f0f7c731617940318925fce4a4b34efe624a6926
psh950 <psh950@naver.com>
2024-06-21 00:25:10 +0900

    빈 등록 초기화 소멸

--- src/test/java/com/example/springbootac/lifecycle/NetworkClient.java
+++ src/test/java/com/example/springbootac/lifecycle/NetworkClient.java
@@ -1,10 +1,6 @@
 package com.example.springbootac.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -32,14 +28,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + this.url) ;
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
