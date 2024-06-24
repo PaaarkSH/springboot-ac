1325ccc1dbb561bc81a63e9c39d0261ea6cbc96e
psh950 <psh950@naver.com>
2024-06-25 00:37:29 +0900

    request 스코프

---
+++ src/main/java/com/example/springbootac/web/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.springbootac.web;
+
+
+import com.example.springbootac.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController  {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL( requestURL);
+        myLogger.log("contoller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
