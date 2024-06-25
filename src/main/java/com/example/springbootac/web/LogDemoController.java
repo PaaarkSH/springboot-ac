d146a871783eb2c46e001a108893440bc8f1ccbf
psh950 <psh950@naver.com>
2024-06-25 20:35:25 +0900

    provider

--- src/main/java/com/example/springbootac/web/LogDemoController.java
+++ src/main/java/com/example/springbootac/web/LogDemoController.java
@@ -4,21 +4,24 @@ package com.example.springbootac.web;
 import com.example.springbootac.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
-public class LoginController  {
+public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger > myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger  = myLoggerProvider.getObject();
         myLogger.setRequestURL( requestURL);
+
         myLogger.log("contoller test");
         logDemoService.logic("testId");
         return "OK";
